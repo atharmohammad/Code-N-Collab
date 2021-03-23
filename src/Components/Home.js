630da891f91169192792c227615a4c54c6817f57
Adnan <adnanshamsi023@gmail.com>
2021-03-23 19:48:52 +0530

    removed 3 lines

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -68,7 +68,3 @@ export default function Home() {
   );
 }
 
-// Add this in your component file
-require("react-dom");
-window.React2 = require("react");
-console.log(window.React1 === window.React2);
