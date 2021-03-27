402c0d4a6532ddc05b3b891989b4ab8a8903d598
Adnan <adnanshamsi023@gmail.com>
2021-03-27 20:19:11 +0530

    proxy on package.json and code for compiler added

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,5 +1,6 @@
-import React,{useEffect} from "react";
+import React, { useEffect, useRef } from "react";
 import AceEditor from "react-ace";
+import axios from "axios";
 
 import "ace-builds/src-noconflict/mode-c_cpp";
 import "ace-builds/src-noconflict/mode-python";
@@ -17,41 +18,62 @@ import {
   FormControl,
   InputLabel,
   Select,
-  MenuItem,Grid
+  MenuItem,
+  Grid,
 } from "@material-ui/core";
-
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-}));
-
-const changeHandler = (e) => {
-  console.log(e);
-};
 
 //main
 const Editor = (props) => {
-  const classes = useStyles();
+  const editorRef = useRef();
+
+  const goCompile = async (e) => {
+    console.log("Compiling",editorRef.current.editor.getValue());
+    const url = "/execute";
+
+    const sendData = {
+      clientId: "d4b7771b3992895017e5ac5f42ec46e6",
+      clientSecret:
+        "37f00b6e1c5f23675ff6bd195a0e6d6631b9f8384dd9c25d1a82a5d274256db3",
+      script: editorRef.current.editor.getValue(),
+      stdin: "",
+      language: "cpp14",
+      versionIndex: "3",
+    };
+
+    try {
+      const response = await axios({
+        method: "post", //you can set what request you want to be
+        url,
+        mode: "no-cors",
+        data: sendData,
+        headers: {
+          "Access-Control-Allow-Methods": "*",
+          "Content-Type": "application/json",
+        },
+      });
+
+      console.log("sucessfull", response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
-    <Grid style={{minHeight:'70vh'}}>
-        <AceEditor
-          mode="c_cpp"
-          theme="monokai"
-          fontSize={16}
-          height="100%"
-          width="100%"
-          onChange={changeHandler}
-          enableLiveAutocompletion={true}
-          name="UNIQUE_ID_OF_DIV"
-          editorProps={{ $blockScrolling: true }}
-        />
-      </Grid>
+    <Grid style={{ minHeight: "70vh" }}>
+      <button onClick={goCompile}>run</button>
+
+      <AceEditor
+        ref={editorRef}
+        mode="c_cpp"
+        theme="monokai"
+        fontSize={20}
+        height="100%"
+        width="100%"
+        enableLiveAutocompletion={true}
+        name="UNIQUE_ID_OF_DIV"
+        editorProps={{ $blockScrolling: true }}
+      />
+    </Grid>
   );
 };
 
