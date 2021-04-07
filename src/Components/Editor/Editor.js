35d11839985254fe09ce69436591f539acdd4bab
atharmohammad <mohd.rule123@gmail.com>
2021-04-07 15:59:49 +0530

    update

--- src/Components/Editor/Editor.js
+++ src/Components/Editor/Editor.js
@@ -13,35 +13,34 @@ const MonacoEditor = () => {
     MonacoEditorRef.current = editor;
   };
 
-  useEffect(() => {
-    Convergence.connectAnonymously(CONVERGENCE_URL, 'Athar')
-      .then((domain) => {
-        return domain.models().openAutoCreate({
-          collection: "Code-n-Collab`",
-          id: '2',
-          ephemeral: false,
-          data: { text: code },
-        });
-      })
-      .then((model) => {
-        console.log(MonacoEditorRef.current);
-        const adapter = new MonacoConvergenceAdapter(MonacoEditorRef.current, model.elementAt("text"));
-        adapter.bind();
-      })
-      .catch((error) => {
-        console.error("Could not open model ", error);
+  useEffect(async() => {
+    try{
+      const domain = await Convergence.connectAnonymously(CONVERGENCE_URL, 'Athar');
+      const modelService = domain.models();
+
+      const model = await modelService.openAutoCreate({
+        collection: "Code-n-Collab`",
+        id: '2',
+        ephemeral: false,
+        data: { text: code },
       });
+
+      const adapter = new MonacoConvergenceAdapter(MonacoEditorRef.current, model.elementAt("text"));
+      adapter.bind();
+    }catch(error){
+      console.error("Could not open model ", error);
+    };
   }, []);
 
   useEffect(() => {
     MonacoEditorRef.current?.setValue("");
   }, []);
 
-  try{
-    console.log(MonacoEditorRef.StandaloneCodeEditor.getValue());
-  }catch(e){
-    console.log(e);
-  }
+  // try{
+  //   console.log(MonacoEditorRef.StandaloneCodeEditor.getValue());
+  // }catch(e){
+  //   console.log(e);
+  // }
 
   return (
     <div style={{ flexGrow: 1, overflow: "hidden" }}>
