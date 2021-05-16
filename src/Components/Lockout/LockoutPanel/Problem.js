dd2899caea9199df809a14ba9cf3fa58f2c75132
atharmohammad <mohd.rule123@gmail.com>
2021-05-16 15:39:37 +0530

    color change according to solved

--- src/Components/Lockout/LockoutPanel/Problem.js
+++ src/Components/Lockout/LockoutPanel/Problem.js
@@ -2,19 +2,33 @@ import { Grid } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import classes from "./lockout.module.css";
 import { connect } from "react-redux";
+import {useLocation} from "react-router-dom";
 
 const Problem = (props) => {
+
+  const location = useLocation();
+
   const problems = props.contest.Problems.map((problem) => {
-    return (
-      <a
+    let bgcolor = "#3959d4";
+
+    if(problem.solved){
+      if(problem.author === location.state.Name){
+        bgcolor = "green"
+      }else{
+        bgcolor = "#f00505"
+      }
+    }
+      return (
+        <a
+        style={{backgroundColor:`${bgcolor}`}}
         className={classes.anchorTag}
         href={problem.link}
         title={problem.name}
         target="_blank"
-      >
+        >
         {problem.points}
-      </a>
-    );
+        </a>
+      );
   });
 
   return (
