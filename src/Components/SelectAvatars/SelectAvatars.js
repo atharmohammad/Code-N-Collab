43fd78e8ca5d6b155344a56fb392dd5166edd64b
Adnan <adnanshamsi023@gmail.com>
2021-06-05 20:31:54 +0530

    resuable select avatar

---
+++ src/Components/SelectAvatars/SelectAvatars.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState, useRef } from "react";
+import Amongus1 from "../../Assets/images/amongus1.png";
+import Amongus2 from "../../Assets/images/amongus2.png";
+import Amongus3 from "../../Assets/images/amongus3.png";
+import Amongus4 from "../../Assets/images/amongus4.png";
+import Amongus5 from "../../Assets/images/amongus5.png";
+import Amongus6 from "../../Assets/images/amongus6.png";
+import Amongus7 from "../../Assets/images/amongus7.png";
+
+const AvatarArray = [
+  Amongus1,
+  Amongus2,
+  Amongus3,
+  Amongus4,
+  Amongus5,
+  Amongus6,
+  Amongus6,
+  Amongus7,
+];
+const Avatar = (props) => {
+  return AvatarArray[props.key];
+};
+
+export default Avatar;
