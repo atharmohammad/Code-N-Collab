2e0bbc54f1bf3b32e0872e5aae73d20e13c7f6cc
atharmohammad <mohd.rule123@gmail.com>
2021-06-05 21:59:24 +0530

    resolved conflicts in update profile

--- src/Pages/UpdateUser.js
+++ src/Pages/UpdateUser.js
@@ -86,13 +86,22 @@ const UpdateUser = (props) => {
         <Nav />
         {startSpinner ? <Spinner /> : (
           <div
+          style={{
+            margin: "auto",
+            padding: "20px",
+            minHeight: "70%",
+            width: "70%",
+            border: "10px double #fff",
+            borderRadius: "20px",
+          }}
+        >
+          <div
             style={{
               margin: "auto",
               padding: "20px",
               minHeight: "70%",
-              width: "70%",
-              border: "10px double #fff",
               borderRadius: "20px",
+              background: "#fff",
             }}
           >
             <div style={{ display: "flex", justifyContent: "space-between" }}>
@@ -108,152 +117,161 @@ const UpdateUser = (props) => {
                     src={SelectAvatar(amongusChar)}
                     alt="avatar"
                     style={{
-                      fontSize: "12px",
-                      height: "30px",
-                      width: "130px",
-                      background: "#8e33bc",
-                      color: "#fff",
+                      height: "120px",
+                      width: "110px",
+                      borderRadius: "10px",
                     }}
-                  >
-                    Choose Avatar
-                  </Button>
+                  />
                 </div>
-                <img
-                  src={ProfileUpdate}
-                  style={{ alignSelf: "center" }}
-                  alt="profile"
-                />
-
                 <Button
+                  onClick={() => setShowAvatarModal(true)}
                   style={{
-                    height: "20%",
-                    padding: "5px 10px 5px 10px",
-                    background: "#336abc",
+                    fontSize: "12px",
+                    height: "30px",
+                    width: "130px",
+                    background: "#8e33bc",
                     color: "#fff",
-                    top: "0",
                   }}
-                  onClick={updateProfile}
                 >
-                  Update
+                  Choose Avatar
                 </Button>
               </div>
-              <div>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <UpdateFeild
-                    width="25%"
-                    value={name}
-                    title="Name"
-                    placeHolder="Adnan Shamsi"
-                    changeHandler={(e) => setName(e)}
-                  />
+              <img
+                src={ProfileUpdate}
+                style={{ alignSelf: "center" }}
+                alt="profile"
+              />
 
-                  <UpdateFeild
-                    width="50%"
-                    value={institution}
-                    title="Institution"
-                    placeHolder="IIT"
-                    changeHandler={(e) => setInstitution(e)}
-                  />
-                  <CountryFeild
-                    width="20%"
-                    country={country}
-                    changeHandler={(e) => setCountry(e)}
-                  />
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <UpdateFeild
-                    width="48%"
-                    value={designation}
-                    title="Designation"
-                    placeHolder="web developer"
-                    changeHandler={(e) => setDesignation(e)}
-                  />
-                  <UpdateFeild
-                    width="48%"
-                    value={moto}
-                    title="Moto"
-                    changeHandler={(e) => setMoto(e)}
-                  />
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <UpdateFeild
-                    width="30%"
-                    value={codeForcesHandle}
-                    title="Codeforces Handle"
-                    placeHolder="Tourist"
-                    changeHandler={(e) => setCodeForcesHandle(e)}
-                  />
-                  <UpdateFeild
-                    width="67%"
-                    value={codeForcesLink}
-                    title="CodeForces Profile Link"
-                    placeHolder="https://codeforces.com/profile/tourist"
-                    changeHandler={(e) => setCodeForcesLink(e)}
-                  />
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <UpdateFeild
-                    width="48%"
-                    value={codechefLink}
-                    title="Codechef Profile Link"
-                    placeHolder="https://www.codechef.com/users/gennady.korotkevich"
-                    changeHandler={(e) => setCodechefLink(e)}
-                  />
-                  <UpdateFeild
-                    width="48%"
-                    value={atcoderLink}
-                    title="Atcoder Profile Link"
-                    placeHolder="https://www.codechef.com/users/gennady.korotkevich"
-                    changeHandler={(e) => setAtcoderLink(e)}
-                  />
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <UpdateFeild
-                    width="48%"
-                    value={githubLink}
-                    title="Github Profile Link"
-                    changeHandler={(e) => setGithubLink(e)}
-                  />
-                  <UpdateFeild
-                    width="48%"
-                    value={linkedInLink}
-                    title="LinkedIn Profile Link"
-                    changeHandler={(e) => setLinkedInLink(e)}
-                  />
-                </div>
+              <Button
+                style={{
+                  height: "20%",
+                  padding: "5px 10px 5px 10px",
+                  background: "#336abc",
+                  color: "#fff",
+                  top: "0",
+                }}
+                onClick={updateProfile}
+              >
+                Update
+              </Button>
+            </div>
+            <div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <UpdateFeild
+                  width="25%"
+                  value={name}
+                  title="Name"
+                  placeHolder="Adnan Shamsi"
+                  changeHandler={(e) => setName(e)}
+                />
+
+                <UpdateFeild
+                  width="50%"
+                  value={institution}
+                  title="Institution"
+                  placeHolder="IIT"
+                  changeHandler={(e) => setInstitution(e)}
+                />
+                <CountryFeild
+                  width="20%"
+                  country={country}
+                  changeHandler={(e) => setCountry(e)}
+                />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <UpdateFeild
+                  width="48%"
+                  value={designation}
+                  title="Designation"
+                  placeHolder="web developer"
+                  changeHandler={(e) => setDesignation(e)}
+                />
+                <UpdateFeild
+                  width="48%"
+                  value={moto}
+                  title="Moto"
+                  changeHandler={(e) => setMoto(e)}
+                />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <UpdateFeild
+                  width="30%"
+                  value={codeForcesHandle}
+                  title="Codeforces Handle"
+                  placeHolder="Tourist"
+                  changeHandler={(e) => setCodeForcesHandle(e)}
+                />
+                <UpdateFeild
+                  width="67%"
+                  value={codeForcesLink}
+                  title="CodeForces Profile Link"
+                  placeHolder="https://codeforces.com/profile/tourist"
+                  changeHandler={(e) => setCodeForcesLink(e)}
+                />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <UpdateFeild
+                  width="48%"
+                  value={codechefLink}
+                  title="Codechef Profile Link"
+                  placeHolder="https://www.codechef.com/users/gennady.korotkevich"
+                  changeHandler={(e) => setCodechefLink(e)}
+                />
+                <UpdateFeild
+                  width="48%"
+                  value={atcoderLink}
+                  title="Atcoder Profile Link"
+                  placeHolder="https://www.codechef.com/users/gennady.korotkevich"
+                  changeHandler={(e) => setAtcoderLink(e)}
+                />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <UpdateFeild
+                  width="48%"
+                  value={githubLink}
+                  title="Github Profile Link"
+                  changeHandler={(e) => setGithubLink(e)}
+                />
+                <UpdateFeild
+                  width="48%"
+                  value={linkedInLink}
+                  title="LinkedIn Profile Link"
+                  changeHandler={(e) => setLinkedInLink(e)}
+                />
               </div>
             </div>
           </div>
+        </div>
         )}
       </div>
     </>
