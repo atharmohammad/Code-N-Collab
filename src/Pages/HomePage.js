13e7fc0fcedc3a6f143ab62ab62ce252ae89fae6
Adnan <adnanshamsi023@gmail.com>
2021-06-08 21:25:39 +0530

    snackbar in homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useContext, useState } from "react";
 import HomePageImg from "../Assets/images/HomePageImg.png";
+import Snackbar from "@material-ui/core/Snackbar";
+
 import { useHistory, useLocation } from "react-router-dom";
 import classes from "../Assets/css/wrapstyle.module.css";
 import Button from "../Components/HomePageButtons/Buttons";
+import MuiAlert from "@material-ui/lab/Alert";
+
 import { Grid } from "@material-ui/core";
 import Stars from "../Components/Stars/Stars";
 import { v1 as uuidv1 } from "uuid";
@@ -12,12 +16,17 @@ import axios from "../Axios/axios";
 import Spinner from "../Components/Spinner/BlogSpinner";
 import { AuthContext } from "../context/auth-context";
 
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 function HomePage() {
   const history = useHistory();
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const auth = useContext(AuthContext);
   const [startSpinner, setSpinner] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(async () => {
     if (searchParams.get("code")) {
@@ -31,11 +40,17 @@ function HomePage() {
         auth.login(data.data.user, data.data.token);
         if (data.data.Way === "signup") {
           history.push("/updateUser");
+        } else {
+          history.push("/homepage");
         }
       } catch (e) {
         console.log("error", e);
       }
       setSpinner(false);
+    }
+
+    if (location.state && location.state.error) {
+      setError(location.state.error);
     }
   }, []);
 
@@ -55,7 +70,7 @@ function HomePage() {
     if (auth.token) {
       return history.push("/me");
     }
-    history.push({ pathname: "/homepage", state: { error: "plz login" } });
+   setError('Plz first login');
   };
 
   const contestHandler = () => {
@@ -67,39 +82,51 @@ function HomePage() {
   };
 
   return (
-    <div
-      style={{
-        height: "100vh",
-        background: "radial-gradient(ellipse, #1b2735 0%, #090a0f 100%)",
-      }}
-    >
-      <Stars color="#fff" />
-      <Back clicked={homePageHandler} />
-      {startSpinner ? (
-        <Spinner />
-      ) : (
-        <>
-          <Nav />
-          <Grid
-            container
-            direction="column"
-            justify="center"
-            alignItems="center"
-            style={{ minHeight: "80vh" }}
-          >
-            <img
-              src={HomePageImg}
-              alt="Code-N-Collab"
-              style={{ marginBottom: "5vh" }}
-            />
-            <Button name="Code - Editor" clicked={roomHandler} />
-            <Button name="LockOut - Championship" clicked={contestHandler} />
-            <Button name="Blogs" clicked={blogHandler} />
-            <Button name="Profile" clicked={profileHandler} />
-          </Grid>
-        </>
-      )}
-    </div>
+    <>
+      <div
+        style={{
+          height: "100vh",
+          background: "radial-gradient(ellipse, #1b2735 0%, #090a0f 100%)",
+        }}
+      >
+        <Stars color="#fff" />
+        <Back clicked={homePageHandler} />
+        {startSpinner ? (
+          <Spinner />
+        ) : (
+          <>
+            <Nav />
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              alignItems="center"
+              style={{ minHeight: "80vh" }}
+            >
+              <img
+                src={HomePageImg}
+                alt="Code-N-Collab"
+                style={{ marginBottom: "5vh" }}
+              />
+              <Button name="Code - Editor" clicked={roomHandler} />
+              <Button name="LockOut - Championship" clicked={contestHandler} />
+              <Button name="Blogs" clicked={blogHandler} />
+              <Button name="Profile" clicked={profileHandler} />
+            </Grid>
+          </>
+        )}
+      </div>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        open={error !== null}
+        autoHideDuration={6000}
+        onClose={() => setError(null)}
+      >
+        <Alert onClose={() => setError(null)} severity="error">
+          {error}
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
 
